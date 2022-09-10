912ec3362a8a50f8ab103d904bd455174e68abe8
Anshaj Vats <9931633906vats@gamil.com>
2022-09-10 15:35:10 +0000

    for checking

--- project02_asm.s
+++ project02_asm.s
@@ -9,17 +9,19 @@ upper_case_return:
 
 	li t0, 32		# t0 = 32
 
+	la t5, (a0)
+
 loop:
 	lb t1, (a0)	# t1 = *a0
 
 	beq t1, zero, done 
-	sub t2, t1, t0
+	sub t2, t1, t0		# convertind 'f' to 'F'	
+	sw t2, (a0)			# saves t2 at a[0]
 	addi a0, a0, 1		# moves a0 to point to next char
 	j loop	
 
 
 
 done:
-
-	mv t2, a0
+	mv a0, t5
 	ret
