0687681648a1099c780c0f2b189d76ad1ae3ea21
Anshaj Vats <9931633906vats@gamil.com>
2022-09-13 05:10:20 +0000

    final commit

--- project02_max_index_asm.s
+++ project02_max_index_asm.s
@@ -8,12 +8,12 @@
 max_index_return:
 	li t0, 0			#loopindex variable
 	li t2, 0			# stores max index
-	li t3, 0			# stores max number
+	li s4, 0			# stores max number
 
 loop:
 	beq t0, a1, done
-	lw t1, (a0)				# t1 = *a0
-	blt t3, t1, continue 	# t3 < t1
+	lw s3, (a0)				# t1 = *a0
+	blt s4, s3, continue 	# t3 < t1
 	addi t0, t0, 1
 	addi a0, a0,4
 	j loop
@@ -22,7 +22,7 @@ loop:
 	
 
 continue:
-	mv t3, t1
+	mv s4, s3
 	mv t2, t0
 	j loop
 
--- project02_sort_asm.s
+++ project02_sort_asm.s
@@ -18,12 +18,13 @@ sort_array:
 	li t0, 0			#index for the loop
 	li t2, 0			# stores max index
 	li s2, 4			# help move index
+	mv s6, a0
 	li t4, 0			# index of the largest number from start of the array
 	li t5, 0			# largest number in array 
 	li t6, 0			# hold arr[0]
 
 loop:
-	beq t0, a1, done
+	bge t0, a1, done
 	sd a0, 8(sp)		#preserve a0	
 	sd a1, 16(sp)		#preserve a1
 	sd t0, 24(sp)		#preserve t0
@@ -41,14 +42,10 @@ loop:
 	
 	mul t4, s2, t2
 
-	ADD a0, t4, a0
-	li t3, 0
+	ADD s5, t4, a0
 
-	lw t5, (a0)
-	
-	sd a0, 32(sp)		# put the addresh on stack
-	
-	ld a0, 8(sp)		#restore a0
+
+	lw t5, (s5)
 
 	lw t6, (a0)
 	
@@ -59,12 +56,10 @@ loop:
 
 swap:
 	sw t5, (a0)
-	ld a0, 32(sp)		#restore a0[32]
-	sw t6, (a0)
-	ld a0, 8(sp)		#restore a0
+	sw t6, (s5)
 	addi a0, a0,4
 	addi t0, t0, 1
-	
+	addi a1, a1, -1	
 	j loop
 
 
